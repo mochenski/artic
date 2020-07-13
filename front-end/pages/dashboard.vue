52c25afc0528ff1268334af2aba01825e453450c
lemochenskif <lemochenskif@gmail.com>
2020-07-13 14:19:45 -0300

    #7 feat: Added dashboard welcome page

--- front-end/pages/dashboard.vue
+++ front-end/pages/dashboard.vue
@@ -1,14 +1,42 @@
 <template>
-  <v-container class="fill-height" height="100%">
-    <v-row>
-      <h1>Welcome Jon Doe</h1>
+  <v-container class="fill-height">
+    <v-row justify="center">
+      <v-col cols="12">
+        <v-row>
+          <div class="text-h1">Welcome {{$auth.user.name}}</div>
+        </v-row>
+        <v-row>
+          <div class="text-h4">This is your dashboard</div>
+        </v-row>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
-  layout: 'dashboard'
+  layout: 'dashboard',
+
+  mounted() {
+    this.loadUser()
+  },
+
+  data: () => ({
+    user: Object
+  }),
+
+  methods: {
+    async loadUser() {
+      // this.$axios
+      //   .get('/articles')
+      //   .then((response) => {
+      //     this.articles = response.data
+      //   })
+      //   .catch((err) => {
+      //     console.log(err.response)
+      //   })
+    }
+  }
 }
 </script>
 
